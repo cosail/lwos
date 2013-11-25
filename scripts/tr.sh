e5b504e998d2192fb3485b70addfca84c1cf843b
Free Share <z8726@163.com>
2013-11-25 17:02:49 +0800

    添加一个把DOS换行符转换成LINUX换行符的脚本。

---
+++ scripts/tr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# for file in `ls -p | grep "[^/]$"; do d2u $file; done
+
+function list_directory()
+{
+	for file in `ls`
+	do
+		if test -d $file
+		then
+#			echo $file is directory.
+			cd $file
+			list_directory
+			cd ..
+		else
+#			echo $file is file.
+			if file $file | grep -q "ASCII"
+			then
+#				echo "d2u $file"
+				d2u $file
+			fi
+		fi
+	done
+}
+
+list_directory
